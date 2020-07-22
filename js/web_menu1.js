1a6f0cb0411de11f2f698c2608a395fc59a81c2f
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-07-22 23:49:29 +0300

    a function to print info and to fo for input validation

--- js/web_menu1.js
+++ js/web_menu1.js
@@ -4,21 +4,28 @@ getTermInputs = () => {
      startDate = document.querySelector('#term-start-date').value;
      endDate = document.querySelector('#term-end-date').value;
 }
+printInputs = () => {
+    document.querySelector('#term-exp-content').innerHTML = '<strong>Dönem Açıklaması</strong><br>' + termExp;
+    document.querySelector('#term-start-date-content').innerHTML = '<strong>Dönem Başlangıç Tarihi</strong><br>' + startDate;
+    document.querySelector('#term-end-date-content').innerHTML = '<strong>Dönem Bitiş Tarihi </strong><br>' + endDate;
+}
+termValidation = () => {
+    /*
+    //to do
+    ->inputs are ok acc to current time?
+    ->if not, warnings.
+    */
+}
 const addTermButton = document.querySelector('#add-term-button');
 addTermButton.addEventListener('click',function(){
     getTermInputs();
-    document.querySelector('#term-exp-content').innerHTML = 'Dönem Açıklaması<br>' + termExp;
-    document.querySelector('#term-start-date-content').innerHTML = 'Dönem Başlangıç Tarihi<br>' + startDate;
-    document.querySelector('#term-end-date-content').innerHTML = 'Dönem Bitiş Tarihi <br>' + endDate;
-
+    printInputs();
 });
 document.querySelectorAll('.term-input').forEach(item => {
     item.addEventListener('keypress',function(e){
         if(e.keyCode == 13){
             getTermInputs();
-            document.querySelector('#term-exp-content').innerHTML = 'Dönem Açıklaması<br>' + termExp;
-            document.querySelector('#term-start-date-content').innerHTML = 'Dönem Başlangıç Tarihi<br>' + startDate;
-            document.querySelector('#term-end-date-content').innerHTML = 'Dönem Bitiş Tarihi <br>' + endDate;
+            printInputs(); 
         }     
     })
 })

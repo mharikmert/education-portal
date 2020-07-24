d1ed5b911b60b9719ffd5ed2afce17c134264689
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-07-24 23:31:04 +0300

    after clicking or pressing enter clear the input boxes

--- js/web_menu1.js
+++ js/web_menu1.js
@@ -1,16 +1,16 @@
-var termExp,startDate,endDate;
+var termExp,startDate,endDate
 getTermInputs = () => {
      termExp = document.querySelector('#term-exp').value;
      startDate = document.querySelector('#term-start-date').value;
      endDate = document.querySelector('#term-end-date').value;
 }
-printInputs = () => {
+displayInfo = () => {
     document.querySelector('#term-exp-content').innerHTML = '<strong>Dönem Açıklaması</strong><br>' + termExp;
     document.querySelector('#term-start-date-content').innerHTML = '<strong>Dönem Başlangıç Tarihi</strong><br>' + startDate;
     document.querySelector('#term-end-date-content').innerHTML = '<strong>Dönem Bitiş Tarihi </strong><br>' + endDate;
     document.querySelector('.switch').style.display = 'block';
     document.querySelector('#create-class-btn').style.display = 'block';
-
+    document.querySelectorAll('.term-input').forEach(item=>{item.value = '';});
 }
 termValidation = () => {
     /*
@@ -22,13 +22,13 @@ termValidation = () => {
 const addTermButton = document.querySelector('#add-term-button');
 addTermButton.addEventListener('click',function(){
     getTermInputs();
-    printInputs();
+    displayInfo();
 });
 document.querySelectorAll('.term-input').forEach(item => {
     item.addEventListener('keypress',function(e){
         if(e.keyCode == 13){
             getTermInputs();
-            printInputs();
+            displayInfo();
         }
     })
 })

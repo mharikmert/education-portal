546a3ee553de12c8e239863a4990db59c043188b
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-07-28 12:58:22 +0300

    second commit of term validation

--- js/web_menu1.js
+++ js/web_menu1.js
@@ -1,4 +1,4 @@
-var termExp,startDate,endDate,termYear1,termYear2;
+var termExp,startDate,endDate,firstYear,secondYear;
 getTermInputs = () => {
      termExp = document.querySelector('#term-exp').value;
      startDate = document.querySelector('#term-start-date').value;
@@ -13,27 +13,29 @@ displayInfo = () => {
     document.querySelector('#create-class-btn').style.display = 'block';
 }
 termValidation = (input) => {
-      var parse = input.split('/');
-      var day = parse[0], month = parse[1], year = parse[2];
-      var parse2 = input.split('-');
-      if(parse2[1] !== undefined){
-        var firstY = parse2[0], secondY = parse2[1];
-      }
-      return day > 0 && day <= 31 && month > 0 && month <= 12;
+      var parseSlash = input.split('/'),parseDash = input.split('-');
+      var day = parseSlash[0], month = parseSlash[1], year = parseSlash[2];
+      if(input === startDate)firstYear = year;
+      if(input === endDate)secondYear = year;
+      var day_monthValidation = day > 0 && day <= 31 && month > 0 && month <= 12;
+      return input === termExp ? parseDash[1]-parseDash[0] === 1 : day_monthValidation;
 }
 const addTermButton = document.querySelector('#add-term-button');
 addTermButton.addEventListener('click',function(){
     getTermInputs();
-    displayInfo();
+    if(termValidation(termExp) && termValidation(startDate) && termValidation(endDate))
+        displayInfo();
 });
 document.querySelectorAll('.term-input').forEach(item => {
     item.addEventListener('keypress',function(e){
         if(e.keyCode == 13){
             getTermInputs();
-            displayInfo();
+            if(termValidation(termExp) && termValidation(startDate) && termValidation(endDate))
+                displayInfo();
         }
     })
 })
+
 /*
 //to do
 ->adding one term div under the other term div respectively.

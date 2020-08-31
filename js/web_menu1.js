ceba8ca48e49758be16b0f7299b55f5ead055248
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-08-31 12:01:03 +0300

    defining an object instead of all of the seperate values

--- js/web_menu1.js
+++ js/web_menu1.js
@@ -1,15 +1,19 @@
-var termExp,startDate,endDate,firstYear,secondYear,expFirstYear,expSecondYear ;
-var iter = 25;
-var temp = 77;
+var firstYear,secondYear,expFirstYear,expSecondYear ;
+var term = {
+  explanation: '',
+  startDate: '',
+  endDate: '',
+  count: 0
+}
 getTermInputs = () => {
-     termExp = document.querySelector('#term-exp').value;
-     startDate = document.querySelector('#term-start-date').value;
-     endDate = document.querySelector('#term-end-date').value;
+     term.explanation = document.querySelector('#term-exp').value;
+     term.startDate = document.querySelector('#term-start-date').value;
+     term.endDate = document.querySelector('#term-end-date').value;
 }
 displayInfo = () => {
-    document.querySelector('#term-exp-content').innerHTML = '<strong>Dönem Açıklaması</strong><br>' + termExp;
-    document.querySelector('#term-start-date-content').innerHTML = '<strong>Dönem Başlangıç Tarihi</strong><br>' + startDate;
-    document.querySelector('#term-end-date-content').innerHTML = '<strong>Dönem Bitiş Tarihi </strong><br>' + endDate;
+    document.querySelector('#term-exp-content').innerHTML = '<strong>Dönem Açıklaması</strong><br>' + term.explanation;
+    document.querySelector('#term-start-date-content').innerHTML = '<strong>Dönem Başlangıç Tarihi</strong><br>' + term.startDate;
+    document.querySelector('#term-end-date-content').innerHTML = '<strong>Dönem Bitiş Tarihi </strong><br>' + term.endDate;
     document.querySelectorAll('.term-input').forEach(item=>{item.value = '';});
     document.querySelector('#toggle-switch-container').style.display = 'flex';
     document.querySelector('#create-class-btn').style.display = 'block';
@@ -19,15 +23,15 @@ termValidation = (input) => {
       var day = parseSlash[0], month = parseSlash[1], year = parseSlash[2];
       var day_monthValidation = day > 0 && day <= 31 && month > 0 && month <= 12;
       switch(input){
-        case termExp: expFirstYear = parseDash[0], expSecondYear = parseDash[1]; break;
-        case startDate: firstYear = year; break;  //global variables assignment
-        case endDate: secondYear = year; break;
+        case term.explanation: expFirstYear = parseDash[0], expSecondYear = parseDash[1]; break;
+        case term.startDate: firstYear = year; break;
+        case term.endDate: secondYear = year; break;
       }
-      return input === termExp ? parseDash[1]-parseDash[0] === 1 : day_monthValidation;
+      return input === term.explanation ? parseDash[1]-parseDash[0] === 1 : day_monthValidation;
 }
 checkInfo = () => {
     getTermInputs();
-    if(termValidation(termExp) && termValidation(startDate) && termValidation(endDate)){
+    if(termValidation(term.explanation) && termValidation(term.startDate) && termValidation(term.endDate)){
         if(expFirstYear == firstYear && expSecondYear == secondYear){
             var date = new Date();
             if(!date.getFullYear() < firstYear){
@@ -39,25 +43,30 @@ checkInfo = () => {
       }else alert('Lütfen dönem bilgilerini kontrol ediniz!');
     return false;
 }
+var termCounter = 1;
 const addTermButton = document.querySelector('#add-term-button');
 addTermButton.addEventListener('click',function(){
-    checkInfo();
-});
+  if(checkInfo()){
+    displayInfo();
+    cloneDiv();
+
+  }
+      });
+
 document.querySelectorAll('.term-input').forEach(item => {
     item.addEventListener('keypress',function(e){
-        if(e.keyCode == 13){
-          iter += 25;
-          temp += 70;
-            div.style.marginTop= temp + 'px'
-            createClass.style.marginTop = iter + '%'
-            checkInfo()
-        }
-    })
+        // if(e.keyCode == 13){
+        //   if(checkInfo()){
+        //   }
+        // }
 })
-displayInfo()
+})
+cloneDiv = () => {
+  var elem = document.querySelector('#existing-term-container')
+  var clone = elem.cloneNode(true)
+  
+}
+
 const setItem = (name, value) => localStorage.setItem(name,value) //sets the var to local storage
 const removeItem = (name) => localStorage.removeItem(name) //removes from the local storage
 const getItem = (item) => localStorage.getItem(item) // gets
-
-var div = document.querySelector('#existing-term-container')
-var createClass = document.querySelector('#create-class-btn')

98c3b2c09990533b637d6b3e4597a1caf1195223
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-03 12:00:28 +0300

    changes in the script

--- db/process.php
+++ db/process.php
@@ -1,27 +1,23 @@
 <?php
- if($_SERVER["REQUEST_METHOD"] == "POST"){
-   $mysql_host = "localhost";
-   $mysql_username = "root";
-   $mysql_password = "";
-   $mysql_database = "fikirtepedb";
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'fikirtepedb';
 
-   $u_id = filter_var($_POST["user_id"], FILTER_SANITIZE_INT);
-   $u_password = filter_var($POST["user_password"], FILTER_SANITIZE_STRING);
-   $u_branch = filter_var($POST["user_branch"], FILTER_SANITIZE_STRING);
+$con = mysqli_connect($server, $username, $password);
 
-   print "id -> ". $u_name " <br> password -> " . $u_password;
-
-   $msyqli = new mysqli($mysql_host, $mysql_username,$mysql_password,$mysql_database);
-   if($mysqli->contect_error){
-     die('error : ('. $mysqli->connect_errno .')'.$mysqli->connect_error);
-   }
-   $statement = $mysqli->prepare("INSERT INTO kayit_log (user_id, user_password, user_branch) VALUES(?,?,?)");
-   $statement = $mysqli->bind_param('sss', $u_id, $u_password, $u_branch);
-
-   if($statement_>execute()){
-     print "got id -> " . $u_id;
-   }else{
-     print $mysqli->error;
-   }
+if(!$con){
+  echo "connection failed! ";
 }
+if(!mysqli_select_db($con, $db)){
+  echo "database not selected";
+}
+
+$id = $_POST['user_id'];
+$password = $_POST['user_password'];
+$sql = "INSERT INTO kayit_log(id, password) VALUES ('$id', '$password')";
+
+if(!mysqli_query($con, $sql)){
+  echo " not inserted";
+}else echo "inserted";
 ?>

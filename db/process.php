65db9bd470976ec64010067600b37f95fd69c5c7
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-03 17:04:43 +0300

    query changes in php file

--- db/process.php
+++ db/process.php
@@ -1,26 +1,21 @@
 <?php
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$db = 'testdb';
+  $server = 'localhost';
+  $username = 'root';
+  $password = '';
+  $db = 'testdb';
 
-$con = new mysqli($server, $username, $password, $db);
-if(!$con){
-  echo "connection failed! ";
-}else echo "connection succedded ";
+  $conn = mysqli_connect($server, $username, $password, $db) or die("unable to connect");
 
-if(!mysqli_select_db($con, $db)){
-  echo "database not selected";
-}else echo "database -> $db ";
+  $user = $_POST['username'];
+  $pass = $_POST['password'];
+  echo "user : $user , pass : $pass ";
 
-$id = $_POST["user_id"];
-$password = $_POST["user_password"];
-// echo "id : $id password: $password"; //it is working
+  $sql = "INSERT INTO user(username,password)
+  VALUES ($user,$pass)";
 
-$sql = "INSERT INTO user (username, password) VALUES ($id,$password)";
-
-if($con->query($sql) == TRUE){
-  echo "succesfully";
-}else echo "ups not inserted";
-// header("refresh:2; url = web_menu.html");
+  if(mysqli_query($conn, $sql)){
+    echo "inserted";
+  }else {
+    echo "not inserted";
+  }
 ?>

5a3ffb91de4af112d4ec50a08d373be1e6937b4c
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-03 17:04:08 +0300

    new php for the same directory

---
+++ db/login.php
@@ -0,0 +1,7 @@
+<html>
+  <form method = "POST" action = "process.php">
+    username: <input type = "text" name = "username"/>
+    password: <input type = "text" name = "password"/>
+    <input type = "submit" value = "insert" /> 
+  </form>
+</html>

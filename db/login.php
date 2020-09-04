59d9212a3c2629320888695a153321a5ee3f9a26
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-04 11:15:44 +0300

    changing names of inputs

--- db/login.php
+++ db/login.php
@@ -1,7 +1,7 @@
 <html>
   <form method = "POST" action = "process.php">
-    username: <input type = "text" name = "username"/>
-    password: <input type = "text" name = "password"/>
+    username: <input type = "text" name = "user_id"/>
+    password: <input type = "text" name = "user_password"/>
     <input type = "submit" value = "insert" />
   </form>
 </html>

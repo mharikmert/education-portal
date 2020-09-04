2a403059f3d4c59197a170f52de6fd1277e454a0
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-04 11:05:11 +0300

    creating a table with php

--- db/codeWood.php
+++ db/codeWood.php
@@ -1,8 +1,18 @@
 <?php
 $connection = new mysqli('localhost', 'root','');
 $sql = "CREATE DATABASE testCreate";
-if($connection->query($sql) == true){
+if($connection->query($sql)){
   echo "database created succesfully.";
 }else echo "database not created!";
-$sql = "INSERT INTO "
- ?>
+$connection = new mysqli('localhost', 'root','', 'testCreate');
+$table = "CREATE TABLE testTable(
+  tckn INT(11) NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  branch VARCHAR(20) NOT NULL
+)";
+if($connection->query($table)){
+  echo " table test table is created successfully";
+}else echo "error creating table : " . $connection->error;
+$connection->close();
+
+?>

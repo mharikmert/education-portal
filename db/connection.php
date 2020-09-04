88929fbec764a33941a6bd3fe3c2193cf33bd9a5
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-04 16:06:22 +0300

    php deletions

--- db/connection.php
+++
@@ -1,9 +0,0 @@
-<html>
-<form method="POST" action = "loginPage.php">
-  id : <input name = "user_id"/>
-  pass: <input name = "user_password"/>
-  branch : <input name = "user_branch">
-  <input type = "submit" value = "login"/>
-
-</form>
-</html>
--- db/login.php
+++
@@ -1,7 +0,0 @@
-<html>
-  <form method = "POST" action = "process.php">
-    username: <input type = "text" name = "user_id"/>
-    password: <input type = "text" name = "user_password"/>
-    <input type = "submit" value = "insert" />
-  </form>
-</html>

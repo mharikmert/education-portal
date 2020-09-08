056f0b66f6c41b11ba3b25d2dd6e074a89c05fe0
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-08 11:57:26 +0300

    3 data trial for kayit log

--- db/kayit_log.php
+++ db/kayit_log.php
@@ -1,17 +1,21 @@
 <?php
-  $conn = mysqli_connect('localhost', 'root','','FikirtepeDB');
-  if($conn){
-    echo"database connected..";
-  }else echo"database not connected";
+  $conn = new mysqli('localhost', 'root','','FikirtepeDB') or die('unable to connect');
 
-  $user_id = $_POST['user_id'];
-  $user_password= $_POST['user_password'];
-  $user_branch = $_POST['user_branch'];
+  /*$sql = "CREATE TABLE kayit_log(
+      tckn BIGINT(11) NULL,
+      sifre VARCHAR(20) NULL,
+      sube VARCHAR(20) NULL
+  )";
+  if(mysqli_query($conn,$sql)) echo"connection succesfully";
+  else echo "connection failed!"; 
+*/
+    $user_id = $_POST['user_id'];
+    $user_password = $_POST['user_password'];
+    $user_branch = $_POST['user_branch'];
 
-  $sql = "INSERT INTO kayit_log(tckn,password,branch)
-  VALUES('$user_id', '$user_password','$user_branch')";
+    $sql = "INSERT INTO kayit_log
+    VALUES('$user_id', '$user_password', '$user_branch')";
 
-  if(mysqli_query($conn,$sql)) {
-    echo "inserted";
-  }else echo "not inserted";
+  if($conn->query($sql)) echo "data inserted";
+    else echo "data not inserted";
  ?>

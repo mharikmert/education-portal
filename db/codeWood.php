25cfbd3e1222cce0fadd687395c036851d3c2fc2
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-08 11:57:01 +0300

    changing the tables to see insertion

--- db/codeWood.php
+++ db/codeWood.php
@@ -5,9 +5,10 @@ if($connection->query($sql)){
   echo "database created succesfully.";
 }else echo "database not created!";
 $connection = new mysqli('localhost', 'root','', 'testCreate');
-$table = "CREATE TABLE testTable(
-  tckn INT(11) NOT NULL,
-  password VARCHAR(20) NOT NULL,
+
+$table = "CREATE TABLE testTable2(
+  tckn BIGINT(11) NOT NULL,
+  sifre VARCHAR(20) NOT NULL,
   branch VARCHAR(20) NOT NULL
 )";
 

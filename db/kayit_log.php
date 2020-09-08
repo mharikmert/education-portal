f606c51c9996256be3bccf5d86eb43934e805b6c
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-09-08 17:42:25 +0300

    unncsy

--- db/kayit_log.php
+++ db/kayit_log.php
@@ -7,14 +7,15 @@
       sube VARCHAR(20) NULL
   )";
   if(mysqli_query($conn,$sql)) echo"connection succesfully";
-  else echo "connection failed!"; 
+  else echo "connection failed!";
 */
     $user_id = $_POST['user_id'];
     $user_password = $_POST['user_password'];
     $user_branch = $_POST['user_branch'];
 
     $sql = "INSERT INTO kayit_log
-    VALUES('$user_id', '$user_password', '$user_branch')";
+    VALUES('$user_id', '$user_password', '$user_branch')
+    WHERE tckn != $user_id";
 
   if($conn->query($sql)) echo "data inserted";
     else echo "data not inserted";

c2f73798c3121558034c6ca9bbf44eb54c6041e2
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-22 21:32:25 +0300

    jpaimpl class cleaned

--- src/main/java/com/fikirtepe/app/DAO/UserRepositoryJpaImplementation.java
+++ src/main/java/com/fikirtepe/app/DAO/UserRepositoryJpaImplementation.java
@@ -1,25 +1,10 @@
 package com.fikirtepe.app.DAO;
 
-import com.fikirtepe.app.Model.User;
-import com.fikirtepe.app.Repository.UserRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Repository
 @Transactional
-public class UserRepositoryJpaImplementation implements UserRepository {
+public class UserRepositoryJpaImplementation  {
 
-    @Override
-    public List<User> getUsers() {
-        return null;
-    }
-
-    @Override
-    public void createUser(User user) {
-
-    }
 }

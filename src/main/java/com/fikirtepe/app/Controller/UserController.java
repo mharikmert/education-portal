8d80cad050923c05ba4b06486c9b6964e3d90963
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-22 21:29:51 +0300

    controller class updated with user creation and get mapping for api/users endpoint

--- src/main/java/com/fikirtepe/app/Controller/UserController.java
+++ src/main/java/com/fikirtepe/app/Controller/UserController.java
@@ -1,13 +1,34 @@
 package com.fikirtepe.app.Controller;
 
 import com.fikirtepe.app.Model.User;
+import com.fikirtepe.app.Repository.UserRepository;
+import com.fikirtepe.app.Service.FikirtepeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    @PostMapping("/api/users")
+    private FikirtepeService fikirtepeService;
+
+    @Autowired
+    public void setFikirtepeService(FikirtepeService fikirtepeService) {
+        this.fikirtepeService = fikirtepeService;
+    }
+
+    @RequestMapping( value = "/api/users", method = RequestMethod.POST)
     public void createUser(@RequestBody User user){
+        user.setName("hilmi");
+        user.setSurname("arikmert");
+
+        fikirtepeService.createUser(user);
         System.out.println(user.toString());
+    }
+    @RequestMapping(value = "api/users", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getUsers(){
+        return ResponseEntity.ok(fikirtepeService.getUsers());
     }
 }

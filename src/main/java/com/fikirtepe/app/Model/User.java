4d43ae53d8285511ecfc63e7d17884eb38424082
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-22 03:23:51 +0300

    new user class

---
+++ src/main/java/com/fikirtepe/app/Model/User.java
@@ -0,0 +1,30 @@
+package com.fikirtepe.app.Model;
+
+public class User{
+    private long id;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}

917e1a66e6074ba0d7bd96381cd094876d6e22c1
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-23 01:42:28 +0300

    enablejpa annotation in main

--- src/main/java/com/fikirtepe/app/FikirtepeApplication.java
+++ src/main/java/com/fikirtepe/app/FikirtepeApplication.java
@@ -2,10 +2,10 @@ package com.fikirtepe.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FikirtepeApplication {
 
     public static void main(String[] args) {

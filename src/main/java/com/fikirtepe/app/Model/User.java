9e801361a60886cad8ed6beb75e47ba6794193c1
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-23 01:43:30 +0300

    adding table name in ıser

--- src/main/java/com/fikirtepe/app/Model/User.java
+++ src/main/java/com/fikirtepe/app/Model/User.java
@@ -4,9 +4,11 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "user")
 public class User{
 
     @Id

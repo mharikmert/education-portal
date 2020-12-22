790c894803c4f36309aad5a7c187eae181d9ae05
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-22 21:31:23 +0300

    UserRepository class to interact database with jpa hibernate

--- src/main/java/com/fikirtepe/app/Repository/UserRepository.java
+++ src/main/java/com/fikirtepe/app/Repository/UserRepository.java
@@ -1,11 +1,7 @@
 package com.fikirtepe.app.Repository;
 
 import com.fikirtepe.app.Model.User;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface UserRepository {
-    List<User> getUsers();
-    void createUser(User user);
+public interface UserRepository extends JpaRepository<User, Long> {
 }

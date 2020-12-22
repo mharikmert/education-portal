cf79f474059a1ddcc9d3d2d7a8bb7c0300db0d26
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-23 01:42:42 +0300

    rest controller cleaned for now

--- src/main/java/com/fikirtepe/app/Web/FikirtepeRestController.java
+++ src/main/java/com/fikirtepe/app/Web/FikirtepeRestController.java
@@ -1,34 +1,4 @@
 package com.fikirtepe.app.Web;
-
-import com.fikirtepe.app.Model.User;
-import com.fikirtepe.app.Service.FikirtepeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-//@RestController
-//RequestMapping("/")
 public class FikirtepeRestController {
-
-//    private final Logger log = LoggerFactory.getLogger(this.getClass());
-//    private final FikirtepeService fikirtepeService;
-//    @Autowired
-//    public FikirtepeRestController(final FikirtepeService fikirtepeService){
-//        this.fikirtepeService = fikirtepeService;
-//    }
-//
-//    @RequestMapping(value = "/users", method = RequestMethod.GET)
-//    public ResponseEntity<List<User>> getUsers(){
-//        List<User> users = fikirtepeService.getUsers();
-//        return ResponseEntity.ok(users);
-//    }
-//    @RequestMapping(value = "/users", method = RequestMethod.POST)
-//    public void createUser(@RequestParam String user){
-//        System.out.println(user);
-//    }
 
 }

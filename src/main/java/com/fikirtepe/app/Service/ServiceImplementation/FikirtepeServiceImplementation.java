c621b23757363cdb7395940a60f4b759291cf265
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-25 14:11:52 +0300

    service class comment

--- src/main/java/com/fikirtepe/app/Service/ServiceImplementation/FikirtepeServiceImplementation.java
+++ src/main/java/com/fikirtepe/app/Service/ServiceImplementation/FikirtepeServiceImplementation.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
+//service layer between user and controller
 @Service
 @Transactional
 public class FikirtepeServiceImplementation implements FikirtepeService {

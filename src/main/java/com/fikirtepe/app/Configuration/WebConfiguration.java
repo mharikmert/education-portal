ef4bbac474b763fc83db709db5791243d765a49f
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-25 14:08:05 +0300

    web configuration class added, it will be corrected later

---
+++ src/main/java/com/fikirtepe/app/Configuration/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.fikirtepe.app.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebConfiguration implements WebMvcConfigurer{
+//
+//    @Bean
+//    public ViewResolver viewResolver(){
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix("/resources/static/text/");
+//        resolver.setSuffix(".html");
+//        return resolver;
+//    }
+}

ab9fd2d468f806a2467fb5d8515d3441f40f8e7c
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-25 14:12:11 +0300

    layout controller for static files' mapping

---
+++ src/main/java/com/fikirtepe/app/Controller/LayoutController.java
@@ -0,0 +1,15 @@
+package com.fikirtepe.app.Controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LayoutController {
+//    @GetMapping(value = "/login", produces = MediaType.TEXT_HTML_VALUE)
+//    @ResponseBody
+//    public String login(){
+//        return "login";
+//    }
+}

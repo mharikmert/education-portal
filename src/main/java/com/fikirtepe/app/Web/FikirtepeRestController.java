e8801f7fdbfb823cdb5522bc2de192ad8006b160
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2020-12-26 17:13:53 +0300

    mapping in rest controller

--- src/main/java/com/fikirtepe/app/Web/FikirtepeRestController.java
+++ src/main/java/com/fikirtepe/app/Web/FikirtepeRestController.java
@@ -1,4 +1,13 @@
 package com.fikirtepe.app.Web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FikirtepeRestController {
+    @RequestMapping("/")
+    public String login(){
+        return "login";
+    }
 
 }

d66f4326e018ed6bb81d01b847047e5b93b64a18
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2021-01-14 18:12:29 +0300

    html files mapping is done

--- src/main/java/com/fikirtepe/app/Controller/LayoutController.java
+++ src/main/java/com/fikirtepe/app/Controller/LayoutController.java
@@ -1,12 +1,26 @@
 package com.fikirtepe.app.Controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@Configuration
 public class LayoutController {
+    @RequestMapping("/")
+    public ModelAndView main(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("main");
+        return mav;
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("login");
+        return mav;
+    }
 
 
 }

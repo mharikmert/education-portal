0a4edffa36d7f548846cd9f499d1cb17e24d9de1
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2021-01-16 20:28:31 +0300

    service methods are updated

--- src/main/java/com/fikirtepe/app/Service/FikirtepeService.java
+++ src/main/java/com/fikirtepe/app/Service/FikirtepeService.java
@@ -3,9 +3,16 @@ package com.fikirtepe.app.Service;
 import com.fikirtepe.app.Model.User;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface FikirtepeService {
-    void createUser(User user);
-    List<User> getUsers();
+
+public interface FikirtepeService{
     User findById(long id);
+    User findByLastName(String lastname);
+    List<User> findAllUsers();
+    void createUser(User user);
+    void delete(User user);
+    boolean verifyUser(User user);
+
+
 }

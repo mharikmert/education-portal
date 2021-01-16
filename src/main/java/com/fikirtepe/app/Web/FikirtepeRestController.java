4e78f4cde7d11f2853fb7ddb04c33700bbd4ee3a
Mahmut Hilmi Arıkmert <mhilmiarikmert@gmail.com>
2021-01-16 20:17:52 +0300

    restController updated

--- src/main/java/com/fikirtepe/app/Web/FikirtepeRestController.java
+++ src/main/java/com/fikirtepe/app/Web/FikirtepeRestController.java
@@ -3,6 +3,7 @@ package com.fikirtepe.app.Web;
 import com.fikirtepe.app.Model.User;
 import com.fikirtepe.app.Service.FikirtepeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,43 +12,44 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class FikirtepeRestController {
 
-    private FikirtepeService fikirtepeService;
-
+    FikirtepeService fikirtepeService;
     @Autowired
     public void setFikirtepeService(FikirtepeService fikirtepeService) {
         this.fikirtepeService = fikirtepeService;
     }
 
     //user info is taken from the post request and user creates
-    @RequestMapping( value = "/api/register", method = RequestMethod.POST)
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
     public void createUser(@RequestBody User user){
-
-        if(fikirtepeService.findById(user.getId()) == null)
+        System.out.println(user.getId());
+        if(fikirtepeService.findById(user.getId()).isEmpty()) {
             fikirtepeService.createUser(user);
-
+            System.out.println(user.toString());
+        }
         else System.out.println("User is already exist");
     }
 
     //get the whole users in system database
-    @RequestMapping(value = "api/users", method = RequestMethod.GET)
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public ResponseEntity<List<User>> getUsers(){
-        return ResponseEntity.ok(fikirtepeService.getUsers());
+        return ResponseEntity.ok(fikirtepeService.findAllUsers());
     }
 
     //check to user info that comes from login screen
-    @RequestMapping(value = "/api/login", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public void checkUser(@RequestBody User user, HttpServletResponse response) throws IOException {
-        System.out.println(user.toString());
+//        System.out.println(user.toString());
         //find the user acc to id
-        User temp = fikirtepeService.findById(user.getId());
 
         //nullity check and also password validation
-        if(temp != null && temp.getPassword().equals(user.getPassword())) {
+        if(fikirtepeService.verifyUser(user)){
             System.out.println("User is verified !");
             //take the user inside and redirect the page
-            response.sendRedirect("../text/web_menu.html");
+//            response.sendRedirect("../text/web_menu.html");
         }
         else {
             //unauthorized error as response and message

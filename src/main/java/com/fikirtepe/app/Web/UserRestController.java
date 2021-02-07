378e8c87334c98a13e0a4c375ca12502df1cff40
mharikmert <mhilmiarikmert@gmail.com>
2021-02-07 14:00:06 +0300

    syntax edition and exception handling for some requests

--- src/main/java/com/fikirtepe/app/Web/UserRestController.java
+++ src/main/java/com/fikirtepe/app/Web/UserRestController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -24,31 +26,52 @@ public class UserRestController {
 
     //user info is taken from the post request and user creates
     @ResponseStatus(HttpStatus.CREATED) // returned 201 if process is succeeded
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public void createUser(@RequestBody User user, HttpServletResponse response) throws IOException {
-        if(userService.findUser(user.getId()) == null) {
-            userService.createUser(user);
-            System.out.println(user.toString());
+    @RequestMapping(
+            value = "/register",
+            method = RequestMethod.POST)
+    public ResponseEntity<?> createUser(@RequestBody User user,
+                                             HttpServletResponse response) throws IOException {
+        try{
+            userService.findUser(user.getId());
+            System.out.println("User is already exist");
+            return ResponseEntity.created(new URI("/user/id")).build();
         }
-        else response.sendError(409,"user is already exist");
+        catch(UserNotFoundException ex){
+            userService.createUser(user);
+        }
+        catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return null;
     }
-    //get the whole users in system database
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    //returns all the users
+    @RequestMapping(
+            value = "/users",
+            method = RequestMethod.GET)
     public ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok(userService.findAllUsers());
     }
 
-    //get user with the given id
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    public ResponseEntity<User> getUser(@PathVariable long id){
-        return ResponseEntity.ok(userService.findUser(id));
+    //returns user with the given id parameter
+    @RequestMapping(
+            value = "/user/{id}",
+            method = RequestMethod.GET)
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        return Optional
+                .ofNullable(userService.findUser(id))
+                .map(user -> ResponseEntity.ok().body(user)) // 200 ok
+                .orElseGet(() -> ResponseEntity.notFound().build()); // 404 not found
     }
 
     //check to user info that comes from login screen
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void checkUser(@RequestBody User user, HttpServletResponse response) throws IOException {
-        if(user.getId() == 0 || user.getPassword() == null) response.sendError(401, "user info is missing");
+    @RequestMapping(
+            value = "/login",
+            method = RequestMethod.POST)
+    public void checkUser(@RequestBody User user,
+                          HttpServletResponse response) throws IOException {
+        if(user.getId() == 0 || user.getPassword() == null)
+            response.sendError(401, "user info is missing");
         else if(userService.verifyUser(user)){
             System.out.println("User is verified !");
             //return ok, and redirect the user in app.js
@@ -59,18 +82,20 @@ public class UserRestController {
         }
     }
     //deletes a user with id
-    //defining explicit mapping to avoid 405 not supported error -> https://www.baeldung.com/spring-request-method-not-supported-405
-    @RequestMapping(value = "/delete/{id}", method = {RequestMethod.GET, RequestMethod.DELETE})
+    @RequestMapping(
+            value = "/delete/{id}",
+            //defining explicit mapping to avoid 405 not supported error -> https://www.baeldung.com/spring-request-method-not-supported-405
+            method = {RequestMethod.GET, RequestMethod.DELETE})
     @ResponseBody
-    public ResponseEntity<?> deleteUser(@PathVariable Long id) throws UserNotFoundException{
+    public ResponseEntity<?> deleteUser(@PathVariable Long id){
         try{
             //find the user with the given id and delete
             userService.findUser(id);
             userService.deleteUser(id);
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok().body("User is successfully deleted");
 
         }catch(UserNotFoundException ex){
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User is not found to delete"); // 404 user not found
         }catch(Exception ex){
             //other exceptions are caught, build an internal server error
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();

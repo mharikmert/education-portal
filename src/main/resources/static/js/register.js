f24b6cadbef628ee73a6c13be12f810940f7cb63
mharikmert <mhilmiarikmert@gmail.com>
2021-02-11 19:52:59 +0300

    register post request with ajax

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,42 +1,19 @@
-// import { redirect } from "./app.js";
-
-const user = {};
-
-const postRegistrationRequest = () => {
-    const xhr = new XMLHttpRequest();
-    const url = "/api/register"; // localhost:8080 not needed
-    xhr.open("POST",url,true);
-    xhr.setRequestHeader("Content-Type","application/json");
-
-    xhr.onreadystatechange = function(){
-        if(xhr.status === 200){
-            alert('kaydınız alınmıştır..');
-            // redirect("/main");
-        }
-    };
-    //convert the data to json objects
-    const data = JSON.stringify({
-        "first_name": user.first_name,
-        "last_name": user.last_name,
-        "id": user.id,
-        "password": user.password,
-        "phone_number": user.phone_number,
-        "city": user.city,
-        "address": user.address
+$(document).ready(function() {
+    $("#register-button").on('click', function() {
+        console.log("i am in the jquery")
+        $.ajax({
+            url : "localhost:8080/api/register",
+            type : "POST",
+            dataType: 'json',
+            headers : {"Content-Type" : "application/json; charset=utf-8"},
+            data : $('#form').serialize(),
+            success : function(result) {
+                console.log(result)
+            },
+            error : function(xhr, resp, text) {
+                console.log(xhr, resp, text)
+            }
+        });
     });
-    console.log(user);
-    xhr.send(data);
-}
-
-document.querySelector('#register-button').addEventListener('click', function(){
-    user.id = document.querySelector('#userID').value;
-    user.password = document.querySelector('#password').value;
-    user.address = document.querySelector('#user-address').value;
-    user.phone_number = document.querySelector('#user-phone-number').value;
-    user.city = document.querySelector('#user-city').value;
-    user.first_name = document.querySelector('#user-name').value;
-    user.last_name = document.querySelector('#user-surname').value;
-
-    postRegistrationRequest();
-
 });
+

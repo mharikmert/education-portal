79c9a4199610cfccea5e9ca6f71e93936ca962bc
mharikmert <mhilmiarikmert@gmail.com>
2021-02-11 22:38:39 +0300

    finAll is also added to the service implementation class

---
+++ src/main/java/com/fikirtepe/app/Service/CityServiceImplemtation/CityAndDistrictServiceImplementation.java
@@ -0,0 +1,40 @@
+package com.fikirtepe.app.Service.CityServiceImplemtation;
+
+import com.fikirtepe.app.Model.City;
+import com.fikirtepe.app.Model.District;
+import com.fikirtepe.app.Repository.CityRepository;
+import com.fikirtepe.app.Repository.DistrictRepository;
+import com.fikirtepe.app.Service.CityAndDistrictService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Service
+public class CityAndDistrictServiceImplementation implements CityAndDistrictService {
+    DistrictRepository districtRepository;
+
+    @Autowired
+    public void setDistrictRepository(DistrictRepository districtRepository) {
+        this.districtRepository = districtRepository;
+    }
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<District> findAllDistricts(Long cityPlateNo){
+       return districtRepository.findAllByCityPlateNo(cityPlateNo);
+    }
+
+    @Override
+    public List<City> findAllCities(){
+//        return entityManager.createQuery("select cityName from City").getResultList();
+        return cityRepository.findAll();
+    }
+}

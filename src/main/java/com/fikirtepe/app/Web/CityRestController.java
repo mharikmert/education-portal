857af42eaa9a81e94d7faf76d6b94e602470031a
mharikmert <mhilmiarikmert@gmail.com>
2021-02-11 22:40:36 +0300

    put all the cities to an endpoint

--- src/main/java/com/fikirtepe/app/Web/CityRestController.java
+++ src/main/java/com/fikirtepe/app/Web/CityRestController.java
@@ -1,6 +1,6 @@
 package com.fikirtepe.app.Web;
 
-import com.fikirtepe.app.Service.CityServiceImplemtation.CityServiceImplementation;
+import com.fikirtepe.app.Service.CityServiceImplemtation.CityAndDistrictServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,19 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CityRestController {
 
-    CityServiceImplementation cityService;
+    CityAndDistrictServiceImplementation cityService;
 
     @Autowired
-    public void setCityService(CityServiceImplementation cityService) {
+    public void setCityService(CityAndDistrictServiceImplementation cityService) {
         this.cityService = cityService;
     }
 
     //returns whole districts with the same plate no
     @RequestMapping(
-            value = "/districts/{cityPlateNo}",
+            value = "/cities/{cityPlateNo}/districts",
             method = RequestMethod.GET
     )
     public ResponseEntity<?> getAllDistricts(@PathVariable long cityPlateNo){
         return ResponseEntity.ok().body(cityService.findAllDistricts(cityPlateNo));
+    }
+
+    @RequestMapping(
+            value = "/cities",
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<?> getCities(){
+        return ResponseEntity.ok().body(cityService.findAllCities());
     }
 }

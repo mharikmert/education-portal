f11ed10efad8e337b213ccf897a2ec351f5ce0c4
mharikmert <mhilmiarikmert@gmail.com>
2021-02-12 22:58:02 +0300

    city-district selection from db with jquery

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -17,3 +17,33 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function(){
+    $('#city').click(function(){
+        const state = $(this).val(), city = $('#city');
+        //get json from endpoint
+        $.ajax({
+            url : '/api/cities',
+            type  : 'GET',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            success: function(result){
+                /* TO DO
+                * cityNames will make appear in city-container div
+                * after cityName is selected, get districts of the city to district-container
+                * */
+                if(result !== undefined && result.length > 0){
+                    city.find('option').remove();
+                    $(result).each(function(index, element){
+                        city.append($('<option> </option>').attr('value', element).text(element));
+                    })
+                }
+            },
+            error: function (result){
+                console.log('error occurred')
+                console.log(result);
+            }
+        });
+    });
+})

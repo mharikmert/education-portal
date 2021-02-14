f7f0a11f8da913aaa72e9923c89172160fc4ce88
mharikmert <mhilmiarikmert@gmail.com>
2021-02-14 20:19:35 +0300

    dynamically appending to the selection boxes for city and district containers

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     $("#register-button").on('click', function() {
-        console.log("i am in the jquery")
         $.ajax({
             url : "localhost:8080/api/register",
             type : "POST",
             dataType: 'json',
             headers : {"Content-Type" : "application/json; charset=utf-8"},
             data : $('#form').serialize(),
+            /* data json should be edited */
             success : function(result) {
-                console.log(result)
+                console.log(result);
+                redirect('registrationApproval.html');
             },
             error : function(xhr, resp, text) {
                 console.log(xhr, resp, text)
@@ -17,33 +18,31 @@ $(document).ready(function() {
     });
 });
 
+//add all the cities to the #city select box with get request
 $(document).ready(function(){
-    $('#city').click(function(){
-        const state = $(this).val(), city = $('#city');
-        //get json from endpoint
-        $.ajax({
-            url : '/api/cities',
-            type  : 'GET',
-            dataType: 'json',
-            headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            success: function(result){
-                /* TO DO
-                * cityNames will make appear in city-container div
-                * after cityName is selected, get districts of the city to district-container
-                * */
-                if(result !== undefined && result.length > 0){
-                    city.find('option').remove();
-                    $(result).each(function(index, element){
-                        city.append($('<option> </option>').attr('value', element).text(element));
-                    })
-                }
-            },
-            error: function (result){
-                console.log('error occurred')
-                console.log(result);
-            }
-        });
+    $.ajax({
+        url: '/api/cities',
+        type: 'GET',
+        dataType : 'json',
+        headers : {"Content-Type" : "application/json; charset=utf-8"},
+        success: function (result){
+            $.each(result, function(index,value){
+                const option = document.createElement('option');
+                option.text = value['cityName'];
+                $('#city').append(option);
+            });
+        },
+    });
+});
+/* district select box should be filled with selection city */
+$(document).ready(function (){
+    $('#city').change( function() {
+        console.log('city is changed..');
+        let selectedOption = $(this).children('option:selected').val();
+        console.log(selectedOption);
     });
 })
+const redirect = (URL) => {
+    window.location.href = URL;
+    return true;
+}

a73ceeb48e8a7bbfa02432ec38215d1b6754a0ea
mharikmert <mhilmiarikmert@gmail.com>
2021-02-24 23:22:15 +0300

    email password configuration and creating a new bean for password

--- src/main/java/com/fikirtepe/app/Configuration/EmailConfiguration.java
+++ src/main/java/com/fikirtepe/app/Configuration/EmailConfiguration.java
@@ -1,12 +1,38 @@
 package com.fikirtepe.app.Configuration;
 
+import com.fikirtepe.app.Service.EmailServiceImplementation.EmailServiceImplementation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 public class EmailConfiguration {
+    private final Properties properties = new Properties();
+
+    public EmailConfiguration() throws  IOException{
+        this.properties.load(configFile());
+    }
+
+    public FileInputStream configFile(){
+        try{
+            return new FileInputStream("src/main/resources/config.properties");
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
     @Bean(name = "email")
     public String getEmail(){
         return "noreplyfikirtepe@gmail.com";
+    }
+
+    @Bean(name = "email_password")
+    public String getPassword(){
+        return properties.getProperty("EMAIL_PASSWORD");
     }
 }

b9dacc9c909a9f3eea0731a1a5e2ebace534bad3
mharikmert <mhilmiarikmert@gmail.com>
2021-03-27 00:00:52 +0300

    rewritten security config with login page security

--- src/main/java/com/fikirtepe/app/Configuration/SecurityConfiguration.java
+++ src/main/java/com/fikirtepe/app/Configuration/SecurityConfiguration.java
@@ -1,50 +1,52 @@
 package com.fikirtepe.app.Configuration;
 
+import com.fikirtepe.app.Service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private UserDetailsService userDetailsService;
+
+    private AuthenticationService authenticationService;
     @Autowired
-    public void setUserDetailsService(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public void setAuthenticationService(AuthenticationService authenticationService){
+        this.authenticationService = authenticationService;
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-                /* open sources path's for any user*/
-                .antMatchers("/**/favicon.ico",
-                        "/css/**",
-                        "/js/**",
-                        "/assets-img/**",
-                        "/api/register/**",
-                        "/api/cities/**",
-                        "/registration/**", // to be able to access registration pages' static sources
-                        "/", // main screen
-                        "/register", // registration form
-                        "/login").permitAll()
-                //role restriction to avoid direct access by out users
-                .antMatchers("menu").access("hasRole('USER')")
-                /* pages authentication according to user roles*/
-                //.antMatchers("/actuator/**").access("hasRole('ADMIN')")
-                /* Request methods are accessed by just authenticated users*/
-                .anyRequest().authenticated();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        //commence method overwritten to avoid unauth as default
+        http.httpBasic().authenticationEntryPoint((request, response, authException) -> response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()));
+        http
+                //just login endpoint should be authenticated for now
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/auth").authenticated()
+                .and()
+                .authorizeRequests().anyRequest().permitAll();
 
-        /* unauthenticated users redirect login page by default*/
-        http.formLogin()
-                .loginPage("/login") //login path
-                .loginProcessingUrl("/menu") //submit uri
-                .failureForwardUrl("/login.html?loginFailed=true"); //authentication failure
-        http.rememberMe().userDetailsService(userDetailsService); //remember me ability
-//        http.httpBasic(); // enabling basic authentication
+    }
+
+    //authorizes our users -> user detail config is in auth service
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.userDetailsService(authenticationService).passwordEncoder(passwordEncoder());
+    }
+    //password encoder bean
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }

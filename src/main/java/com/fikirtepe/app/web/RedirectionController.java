6fb7a07d4272d36d2b5bb4f79f03309d73c2c643
mhadev <mhilmiarikmert@gmail.com>
2021-06-10 21:52:03 +0300

    create redirection controller

---
+++ src/main/java/com/fikirtepe/app/web/RedirectionController.java
@@ -0,0 +1,38 @@
+package com.fikirtepe.app.web;
+
+import com.fikirtepe.app.model.User;
+import com.fikirtepe.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class RedirectionController {
+    private final UserService userService;
+    @Autowired
+    public RedirectionController(UserService userService){
+        this.userService = userService;
+    }
+    //renders admin menu page with given id
+    @GetMapping("/{userType}-menu/{id}")
+    public ModelAndView redirectUser(@PathVariable String userType, @PathVariable Long id){
+       try{
+           User user = userService.findUser(id);
+           ModelAndView mav = new ModelAndView();
+           mav.addObject(user);
+           mav.setViewName("User Types/" + userType + "-menu");
+           return mav;
+       }
+       catch(Exception ex){
+           ex.printStackTrace();
+           return null;
+       }
+
+       /*TO DO
+        * Editing on error pages
+        * Handling illegal path variables
+        */
+    }
+}

fb896a818caf83066b899106df93f823571bba7e
mhadev <mhilmiarikmert@gmail.com>
2021-06-10 21:49:29 +0300

    refactor in layout controller

--- src/main/java/com/fikirtepe/app/controller/LayoutController.java
+++ src/main/java/com/fikirtepe/app/controller/LayoutController.java
@@ -1,6 +1,8 @@
 package com.fikirtepe.app.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,79 +12,27 @@ import org.springframework.web.servlet.ModelAndView;
 public class LayoutController {
     @RequestMapping("/")
     public ModelAndView main(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("main");
-        return mav;
+        return new ModelAndView("main");
     }
-
     @RequestMapping("/login")
     public ModelAndView login(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("login");
-        return mav;
+        return new ModelAndView("login");
     }
-
-//    @RequestMapping("/forgotPassword")
-//    public ModelAndView forgotPassword(){
-//            ModelAndView mav = new ModelAndView();
-//            mav.setViewName("forgot-password-page");
-//            return mav;
-//    }
-
     @RequestMapping("/register")
     public ModelAndView register(){
-            ModelAndView mav = new ModelAndView();
-            mav.setViewName("register");
-            return mav;
+            return new ModelAndView("register");
     }
-
-    @RequestMapping("/menu")
-    public ModelAndView menu(){
-            ModelAndView mav = new ModelAndView();
-            mav.setViewName("web_menu");
-            return mav;
-    }
-
     @RequestMapping("/approval")
     public ModelAndView approval(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("registrationApproval");
-        return mav;
+        return new ModelAndView("registrationApproval");
     }
     @RequestMapping("/admin")
     public ModelAndView admin(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("Admin/adminPanel");
-        return mav;
+        return new ModelAndView("Admin/adminPanel");
     }
     @RequestMapping("/studentApproval")
     public ModelAndView studentApproval(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("Admin/studentApproval");
-        return mav;
+        return new ModelAndView("Admin/studentApproval");
     }
-    @RequestMapping("/admin-menu")
-    public ModelAndView adminMenu(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("/User Types/admin-menu");
-        return mav;
-    }
-    @RequestMapping("/student-menu")
-    public ModelAndView studentMenu(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("User Types/student-menu");
-        return mav;
-    }
-    @RequestMapping("/teacher-menu")
-    public ModelAndView teacherMenu(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("User Types/teacher-menu");
-        return mav;
-    }
-    @RequestMapping("/parent-menu")
-    public ModelAndView parentMenu(){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("User Types/parent-menu");
-        return mav;
-    }
+
 }

91f3770eb3f9edabb3601310d39c783df8e47af5
mhadev <mhilmiarikmert@gmail.com>
2021-07-07 15:39:20 +0300

    login and authentication functionalities in login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
+import { JsonpInterceptor } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  username?:string; 
+  password?:string; 
 
-  constructor() { }
+  errorType?: string; 
+  btnClicked: boolean = false; 
+
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
+  onSubmit(){
+    this.btnClicked = true; 
 
+    if(!this.username || !this.password){
+      this.errorType = 'empty-data'
+      setTimeout( () => this.btnClicked = false, 1500)
+      return; 
+    }
+
+    let resp  = this.authService.login(this.username, this.password)
+    console.log(resp)
+  }
 }

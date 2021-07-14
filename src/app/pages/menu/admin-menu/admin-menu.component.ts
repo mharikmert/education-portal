d67bb2e41127ebc9bafad14f5ec281e92ff6c91b
mharikmert <mhilmiarikmert@gmail.com>
2021-07-14 11:50:16 +0300

    create user menus

---
+++ src/app/pages/menu/admin-menu/admin-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+@Component({
+  selector: 'app-admin-menu',
+  templateUrl: './admin-menu.component.html',
+  styleUrls: ['./admin-menu.component.css']
+})
+export class AdminMenuComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/menu/student-menu/student-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-menu',
+  templateUrl: './student-menu.component.html',
+  styleUrls: ['./student-menu.component.css']
+})
+export class StudentMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

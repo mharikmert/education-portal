a73b078b68d2daa2d3cd07e8019f7a9eae0ddf8e
mharikmert <mhilmiarikmert@gmail.com>
2021-07-14 11:43:03 +0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginErrorComponent } from './components/login-error/login-error.component';
 import { FormsModule } from '@angular/forms';
+import { ParentMenuComponent } from './pages/menu/parent-menu/parent-menu.component';
+import { AuthorizedMenuComponent } from './pages/menu/authorized-menu/authorized-menu.component';
+import { StudentMenuComponent } from './pages/menu/student-menu/student-menu.component';
+import { TeacherMenuComponent } from './pages/menu/teacher-menu/teacher-menu.component';
+import { AdminMenuComponent } from './pages/menu/admin-menu/admin-menu.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +25,13 @@ import { FormsModule } from '@angular/forms';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    LoginErrorComponent
+    LoginErrorComponent,
+    ParentMenuComponent,
+    AuthorizedMenuComponent,
+    StudentMenuComponent,
+    TeacherMenuComponent,
+    AdminMenuComponent,
+    LogoutButtonComponent
   ],
   imports: [
     BrowserModule,

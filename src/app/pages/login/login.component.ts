21f80af7956586f347adaa1ae8bcab404d58224c
mharikmert <mhilmiarikmert@gmail.com>
2021-08-04 17:53:36 +0300

    Specify the subscribe function with jwt

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -10,12 +11,10 @@ export class LoginComponent implements OnInit {
   
   username?:string; 
   password?:string; 
-
-  errorType?: string; 
+  errorType?: string; //should refactor as boolean  
   btnClicked: boolean = false; 
 
-
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -24,15 +23,27 @@ export class LoginComponent implements OnInit {
 
     if(!this.username || !this.password){
       this.errorType = 'empty-data'
-      setTimeout( () => {this.btnClicked = false, this.errorType = ''}, 1500)
+      this.btnClicked = false
+      setTimeout( () => {this.errorType = ''}, 1500)
       return; 
     }
 
     let resp  = this.authService.login(this.username, this.password)
-    resp.subscribe((response) => {
-      console.log(response)
-      this.btnClicked = false
-    })
 
+    resp.subscribe((response) => {
+      const token = (<any>response).token;     
+      // console.log(token)
+      //puts the JWT to local storage 
+      localStorage.setItem("JWT", token)
+
+      console.log(localStorage.getItem("JWT"))  
+
+      this.btnClicked = false
+      this.router.navigate(['/admin-menu'])
+    
+    }, err => {
+      this.errorType = 'any-error'
+      setTimeout( () => {this.btnClicked = false, this.errorType = '' }, 1500)
+    })
   }
 }

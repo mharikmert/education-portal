6e2801382d3bb133c7137c80bd92fcc72acc6a9d
mharikmert <mhilmiarikmert@gmail.com>
2021-08-04 17:52:28 +0300

    Implement perform logout function

--- src/app/components/logout-button/logout-button.component.ts
+++ src/app/components/logout-button/logout-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout-button',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  performLogout(){
+    localStorage.removeItem("JWT");
+    this.router.navigate(['login'])
   }
-
 }

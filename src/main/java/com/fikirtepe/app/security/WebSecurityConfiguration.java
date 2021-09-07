a2ea8466fc84d58158f6950c521e1c4d3b761bfc
mharikmert <mhilmiarikmert@gmail.com>
2021-09-07 23:31:43 +0300

    Implement jwt security actions, move security config to a new package

--- src/main/java/com/fikirtepe/app/security/WebSecurityConfiguration.java
+++ src/main/java/com/fikirtepe/app/security/WebSecurityConfiguration.java
@@ -1,28 +1,40 @@
-package com.fikirtepe.app.configuration;
+package com.fikirtepe.app.security;
 
-import com.fikirtepe.app.service.AuthenticationService;
+import com.fikirtepe.app.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private AuthenticationService authenticationService;
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private JwtUserDetailsService jwtUserDetailsService;
+    private JwtRequestFilter jwtRequestFilter;
+
     @Autowired
-    public void setAuthenticationService(AuthenticationService authenticationService){
-        this.authenticationService = authenticationService;
+    public void setAuthenticationService(JwtUserDetailsService jwtUserDetailsService, JwtRequestFilter jwtRequestFilter, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint){
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -30,16 +42,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.cors();
         http.csrf().disable();
         //commence method overwritten to avoid unauth as default
-        http.httpBasic().authenticationEntryPoint((request, response, authException) -> response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()));
         http.authorizeRequests()
                 .antMatchers("/", "/register", "/login", "/js/**", "/css/**", "/assets-img/**", "/registration/**", "/approval", "/api/cities/**").permitAll()
                 //allow register and login post requests
                 .antMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/api/auth").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/auth").permitAll()
                 .antMatchers("/swagger-resources/*", "*.html", "/api/v1/swagger.json")
-                    .hasAuthority("ADMIN")
-                .anyRequest().authenticated();
+                .hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
                 .formLogin()
                 .loginPage("/login")
@@ -47,15 +62,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutUrl("/perform_logout")
                 .logoutSuccessUrl("/login");
+        // Add a filter to validate the tokens with every request
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
-    //authorizes our users -> user detail config is in auth service
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth.userDetailsService(authenticationService).passwordEncoder(passwordEncoder());
-    }
+
     //password encoder bean
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 }

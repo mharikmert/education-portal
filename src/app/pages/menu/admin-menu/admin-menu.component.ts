e7c506ae28f2a51606b37a418babe2270a088f63
mharikmert <mhilmiarikmert@gmail.com>
2021-09-10 23:12:58 +0300

    Rewrite method subscriptions communicating with term service

--- src/app/pages/menu/admin-menu/admin-menu.component.ts
+++ src/app/pages/menu/admin-menu/admin-menu.component.ts
@@ -4,6 +4,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Term } from 'src/app/models/Term';
 import { Observable } from 'rxjs';
+import { TermService } from 'src/app/services/term.service';
 
 @Component({
   selector: 'app-admin-menu',
@@ -18,10 +19,19 @@ export class AdminMenuComponent implements OnInit {
   isTermActive: boolean = false; 
   faTimes = faTimes
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private termService: TermService) { }
 
   private apiUrl = 'http://localhost:8080'; 
+  private headers = new HttpHeaders({
+    'Content-Type':'applicatoin/json',
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
   ngOnInit(): void {
+    this.loadPage(); 
+  }
+
+  loadPage(){
+    this.getAdmin();
     this.getTerms();
   }
 
@@ -29,35 +39,13 @@ export class AdminMenuComponent implements OnInit {
     this.termButtonClicked = true
   }
 
-  getAdmin() {
-    const id = localStorage.getItem('id');
-    const httpOptions = {
-      headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      //authenticate user with basic auth 
-      'Authorization': 'Basic ' + btoa('63589915196:password')
-    })
-  }
-    return this.httpClient.get(this.apiUrl + '/api/users', httpOptions)
-  }
-  
+  getAdmin = () => this.termService.getAdmin().subscribe(resp => console.log(resp));;
+
   addTerm(term: Term){ 
-    const httpOptiions = new HttpHeaders({
-      'Content-Tupe': 'application/json',
-      'Authorization':'Basic ' + btoa('63589915196:password')
-    })
-    // const term = new Term(this.termName, this.startDate, this.endDate);
-    this.httpClient.post(this.apiUrl + '/api/terms', term, {headers: httpOptiions}).subscribe(term => this.terms.push(term));
+    this.termService.addTerm(term).subscribe(
+      term => this.terms.push(term)
+      );
   }
 
-  getTerms() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json', 
-        'Authorization': 'Basic ' + btoa('63589915196:password')
-      })
-    }
-    return this.httpClient.get<Term []>(this.apiUrl + '/api/terms', httpOptions).subscribe(terms => this.terms = terms);
-  }
-  
+  getTerms = () => this.termService.getTerms().subscribe(terms => this.terms = terms);
 }

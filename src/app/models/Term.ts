c8f430deb1629deba6c873372007f6c5c83662ce
mharikmert <mhilmiarikmert@gmail.com>
2021-09-14 20:34:37 +0300

    Add isActive field for status of the term

--- src/app/models/Term.ts
+++ src/app/models/Term.ts
@@ -2,10 +2,5 @@ export class Term{
     name?: string; 
     startDate?: Date;  
     endDate?: Date;
-    
-    constructor(name: string, startDate: Date, endDate: Date){
-        this.name = name; 
-        this.startDate = startDate; 
-        this.endDate = endDate; 
-    }
+    active?: boolean; 
 }

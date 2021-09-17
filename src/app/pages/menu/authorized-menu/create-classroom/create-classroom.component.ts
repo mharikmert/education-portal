6e5aae037ff35e0f58977d879ccbf7e2a71ab203
mharikmert <mhilmiarikmert@gmail.com>
2021-09-17 21:51:01 +0300

    Implement create-classroom component

--- src/app/pages/menu/authorized-menu/create-classroom/create-classroom.component.ts
+++ src/app/pages/menu/authorized-menu/create-classroom/create-classroom.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Classroom } from 'src/app/models/Classroom';
+import { ClassroomService } from 'src/app/services/classroom.service';
 
 @Component({
   selector: 'app-create-classroom',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateClassroomComponent implements OnInit {
 
-  constructor() { }
+  classrooms: Classroom [] = [];
+  classroom: Classroom = {}
+  constructor(private classroomService: ClassroomService) { }
 
   ngOnInit(): void {
+    this.getClassroms();
   }
 
+  getClassroms = () => this.classroomService.getClassrooms().subscribe( classrooms => this.classrooms = classrooms)
+
+  addClassroom = (classroom: Classroom) => this.classroomService.addClassroom(classroom).subscribe();
 }

d3fd5c7b697d034d2a49373b27580e1eaabea019
mharikmert <mhilmiarikmert@gmail.com>
2021-09-17 21:39:47 +0300

    Add new user roles

--- src/main/java/com/fikirtepe/app/model/Role.java
+++ src/main/java/com/fikirtepe/app/model/Role.java
@@ -4,8 +4,9 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority  {
 
-    ROLE_ADMIN, ROLE_USER;
-    //user roles as string "ROLE_ADMIN", "ROLE_USER"
+    ROLE_ADMIN, ROLE_STUDENT,ROLE_PARENT,ROLE_TEACHER;
+
+    //returns user roles -> "ROLE_ADMIN"
     @Override
     public String getAuthority() {
         return name();

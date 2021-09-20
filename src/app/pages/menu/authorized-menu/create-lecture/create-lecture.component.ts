4f8247e8c2b41f612b15eaea421259d103333948
mharikmert <mhilmiarikmert@gmail.com>
2021-09-20 22:44:29 +0300

    Implement create-lecture actions

--- src/app/pages/menu/authorized-menu/create-lecture/create-lecture.component.ts
+++ src/app/pages/menu/authorized-menu/create-lecture/create-lecture.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Lecture } from 'src/app/models/Lecture';
+import { LectureService } from 'src/app/services/lecture.service';
 
 @Component({
   selector: 'app-create-lecture',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateLectureComponent implements OnInit {
 
-  constructor() { }
+  lecture: Lecture = {};
+  lectures: Lecture [] = [];
+  constructor(private lectureService: LectureService) { }
 
   ngOnInit(): void {
+    this.getLectures();
   }
+  getLectures = () => this.lectureService.getLectures().subscribe(lectures => this.lectures = lectures);
+
+  addLecture = (lecture: Lecture) => this.lectureService.addLecture(lecture).subscribe(lecture => this.lectures.push(lecture));
 
 }

322ed7d5663d5365b9495c047d292d7cb8a51f00
mharikmert <mhilmiarikmert@gmail.com>
2021-09-20 22:41:30 +0300

    Implement create classroom actions

--- src/app/pages/menu/authorized-menu/create-classroom/create-classroom.component.ts
+++ src/app/pages/menu/authorized-menu/create-classroom/create-classroom.component.ts
@@ -11,6 +11,8 @@ export class CreateClassroomComponent implements OnInit {
 
   classrooms: Classroom [] = [];
   classroom: Classroom = {}
+  strGrade: string = ''; 
+  strCapacity: string = ''; 
   constructor(private classroomService: ClassroomService) { }
 
   ngOnInit(): void {
@@ -19,5 +21,11 @@ export class CreateClassroomComponent implements OnInit {
 
   getClassroms = () => this.classroomService.getClassrooms().subscribe( classrooms => this.classrooms = classrooms)
 
-  addClassroom = (classroom: Classroom) => this.classroomService.addClassroom(classroom).subscribe();
+  addClassroom = (classroom: Classroom) => {
+    this.classroom.capacity = parseInt(this.strCapacity);
+    this.classroom.grade = parseInt(this.strGrade);
+    console.log(this.classroom)
+    this.classroomService.addClassroom(classroom).subscribe(classrom => this.classrooms.push(classrom));
+  }  
+
 }

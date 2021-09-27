eca05bc4b256af0ffa361e555520acd8fda2d065
mharikmert <mhilmiarikmert@gmail.com>
2021-09-27 23:53:25 +0300

    Implement an Observable user to share between unrelated component likewise sharedClassroom

--- frontend/angular/src/app/services/user.service.ts
+++ frontend/angular/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders, HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../models/User';
 
 @Injectable({
@@ -13,10 +13,20 @@ export class UserService {
     'Authorization':'Bearer ' + localStorage.getItem('token')
   })
   
+
+  //shares users between unrelated components 
+  private user : BehaviorSubject<User>= new BehaviorSubject<User>( new User());
+  public sharedUser = this.user.asObservable();
+
   constructor(private httpClient: HttpClient) { }
   
   getUsers(path: string) : Observable<User []> {
     return this.httpClient.get<User []>(`${this.apiUrl}/api/${path}`, {headers: this.headers})
   }
+
+  nextUser(user: User){
+    this.user.next(user);
+  }
+  
 
 }

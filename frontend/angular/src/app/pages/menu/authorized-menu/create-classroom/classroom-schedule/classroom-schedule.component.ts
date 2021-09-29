2a54caa161325e28490e24d49f73e4e286ade4d3
mharikmert <mhilmiarikmert@gmail.com>
2021-09-29 23:13:51 +0300

    Implement filterSchedule method

--- frontend/angular/src/app/pages/menu/authorized-menu/create-classroom/classroom-schedule/classroom-schedule.component.ts
+++ frontend/angular/src/app/pages/menu/authorized-menu/create-classroom/classroom-schedule/classroom-schedule.component.ts
@@ -13,10 +13,23 @@ export class ClassroomScheduleComponent implements OnInit {
   constructor(private classroomService : ClassroomService) { }
   classroom: Classroom = {}
   schedule: Section [] = []
+  days: string [] =  ['Pazartesi', 'Sali', 'Carsamba', 'Persembe', 'Cuma'];
+  startingTime: string [] = ['09:00', '10:00','11:00','12:00','13:00','14:00','15:00', '16:00','17:00','18:00']//,'19:00','20:00','21:00']
 
   ngOnInit(): void {
     this.classroomService.sharedClassrom.subscribe(classroom => this.classroom = classroom);
     this.classroomService.getSchedule(this.classroom.id).subscribe(section => this.schedule = section);
   }
+
+  filterSchedule(startingTime: string, day: string){
+    let lectureName;
+    const filterSchedule: Section [] = this.schedule.filter( schedule => schedule.startingTime === startingTime && schedule.day === day);
+
+    if(filterSchedule.length != 0)
+      filterSchedule.map(element => lectureName = element.lecture?.name );
+    
+    return lectureName;
+  }
+
 
 }

ba159e75130a4a7c3827ecb0f596d96c2ccd37ce
mharikmert <mhilmiarikmert@gmail.com>
2021-10-01 23:06:36 +0300

    Refactor | Detailed implementation of the section for two hour lectures

--- backend/spring/src/main/java/com/fikirtepe/app/controller/ClassroomController.java
+++ backend/spring/src/main/java/com/fikirtepe/app/controller/ClassroomController.java
@@ -5,6 +5,7 @@ import com.fikirtepe.app.model.Section;
 import com.fikirtepe.app.service.ClassroomService;
 import com.fikirtepe.app.service.SectionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,8 +36,24 @@ public class ClassroomController {
     }
 
     @PostMapping("/assignLecture")
-    public ResponseEntity<Section> assignLecture(@RequestBody Section section){
-        return ResponseEntity.ok(sectionService.createSection(section));
+    public ResponseEntity<?> assignLecture(@RequestBody Section section){
+        if(sectionService.findSectionByClassroomIdAndDayAndStartingTime(section.getClassroom().getId(), section.getDay(), section.getStartingTime()) == null){
+            if (section.getNumberOfHours() == 1) {
+               sectionService.createSection(section);
+               return ResponseEntity.status(HttpStatus.CREATED).body(section);
+            }
+            else if (section.getNumberOfHours() == 2) {
+               sectionService.createSection(section);
+               String startingTimeOfNextLecture = Integer.parseInt(section.getStartingTime().split(":")[0]) + 1 + ":00";
+               Section nextSection = new Section(section.getId() + 1, section.getDay(), startingTimeOfNextLecture, section.getNumberOfHours() - 1, section.getLecture(), section.getTeacher(), section.getClassroom());
+               sectionService.createSection(nextSection);
+               return ResponseEntity.status(HttpStatus.CREATED).body(List.of(section, nextSection));
+            }
+            else
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("invalid section");
+        }
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Section is not empty");
     }
 
     @GetMapping("/{id}/schedule")

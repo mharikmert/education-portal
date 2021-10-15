05f49dcc1d716583ea0a99164c45af5e46466139
mharikmert <mhilmiarikmert@gmail.com>
2021-10-15 21:52:37 +0300

    Refactor login actions depending on user roles

--- frontend/angular/src/app/pages/login/login.component.ts
+++ frontend/angular/src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoggedInService } from 'src/app/services/logged-in.service';
 import { TokenService } from 'src/app/services/token.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -20,7 +21,8 @@ export class LoginComponent implements OnInit {
   constructor(private authService: AuthService, 
     private router: Router, 
     private tokenService: TokenService,
-    private loggedInService: LoggedInService) { }
+    private loggedInService: LoggedInService,
+    private userService: UserService) { }
 
   ngOnInit(): void {
   }
@@ -34,18 +36,33 @@ export class LoginComponent implements OnInit {
       return; 
     }
 
-    this.authService.login(this.username, this.password).subscribe((response) => {
-      console.log(response)
+    this.authService.login(this.username, this.password).subscribe(response => {
+      console.log(response) //response includes username, user authorities and access token  
       const token = (<any>response).accessToken;     
       //puts the JWT to local storage 
       this.tokenService.setToken(token)
-      
-      localStorage.setItem('username', this.username);
-
       this.loggedInService.setLoggedInUser(this.username)
 
       this.btnClicked = false
-      this.router.navigate(['/admin-menu'])
+      //only admins have access, not available for all users 
+      // this.userService.getUserByUsername(this.loggedInService.getLoggedInUser()).subscribe( user => {
+      //   }
+
+        //get roles from the login response
+        let roles = (<any>response).authorities;
+
+        //one role condition 
+        if(roles.length === 1){ 
+         const role = roles[0].split('_')[1].toLowerCase(); // role -> admin, student, teacher, parent
+        //  console.log('curront role -> ', role)
+         this.router.navigate([`${role}-menu`])
+        }
+         //multiple roles condition
+        else {
+            //share roles with loggedin component 
+            this.userService.nextRole(roles);
+            this.router.navigate(['logged-in'])
+        }
     
     }, err => {
       this.errorType = 'any-error'

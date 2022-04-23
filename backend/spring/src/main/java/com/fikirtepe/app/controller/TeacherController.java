13fb9571bd8e28457de902e08a97ae0f47e4c2e6
mharikmert <mhilmiarikmert@gmail.com>
2022-04-23 19:31:20 +0300

    BE | Add lecture check

--- backend/spring/src/main/java/com/fikirtepe/app/controller/TeacherController.java
+++ backend/spring/src/main/java/com/fikirtepe/app/controller/TeacherController.java
@@ -1,6 +1,8 @@
 package com.fikirtepe.app.controller;
 
+import com.fikirtepe.app.model.Lecture;
 import com.fikirtepe.app.model.Teacher;
+import com.fikirtepe.app.service.LectureService;
 import com.fikirtepe.app.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,9 +15,11 @@ import java.util.List;
 @RequestMapping("/api/teachers")
 public class TeacherController {
     private final TeacherService teacherService;
+    private final LectureService lectureService;
     @Autowired
-    public TeacherController(TeacherService teacherService){
+    public TeacherController(TeacherService teacherService, LectureService lectureService) {
         this.teacherService = teacherService;
+        this.lectureService = lectureService;
     }
     @GetMapping
     public ResponseEntity<List<Teacher>> getTeachers(){
@@ -25,6 +29,14 @@ public class TeacherController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseEntity<Teacher> createTeacher(@RequestBody Teacher teacher){
+
+        Lecture lecture = lectureService.getLectureByName(teacher.getLecture().getName());
+
+        if(lecture == null)
+            lectureService.createLecture(teacher.getLecture());
+        else
+            teacher.setLecture(lecture);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(teacherService.createTeacher(teacher));
     }
     @PostMapping("/temp")

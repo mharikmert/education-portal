c7bb2232f09c5631f8ef61d78fbe1d441d98f2c1
Mahmut Hilmi Arikmert <mhilmiarikmert@gmail.com>
2022-12-13 23:39:07 +0300

    Create a home controller

---
+++ backend/spring/src/main/java/portal/backend/app/controller/HomeController.java
@@ -0,0 +1,12 @@
+package portal.backend.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping(value = "/")
+    public String home() {
+        return "Portal Backend Application is running successfully..";
+    }
+}
